5248694e3a831b53ed75c72b17d07ece402f744b
unknown <qwerty111223344444@gmail.com>
2025-09-28 04:43:27 +0300

    add Astro deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://qwerty111223344444.github.io',
+  base: '/calvary-website',   // имя репозитория
   output: 'static',
   integrations: [tailwind()],
 });
