402c6844376a9dd89bbdd3a7537e4723d3636d36
unknown <qwerty111223344444@gmail.com>
2025-09-28 19:05:06 +0300

    test fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://qwerty111223344444.github.io',
-  base: '/calvary-website/',   // имя репозитория
+  site: 'https://calvarybaseball-tver.ru',
+  base: '/', 
   output: 'static',
   integrations: [tailwind()],
 });
